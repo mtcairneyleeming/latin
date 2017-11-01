1fb823ec5b05fe85191fb0c9ba45cee69d7d8da0
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-01 19:00:50 +0000

    Add further features to controllers, as well as adding separate controllers for lists and sections, and deleting the superfluous valuescontroller
    Signed-off-by: Max Cairney-Leeming <mtcairneyleeming@gmail.com>

--- api/Controllers/LemmaController.cs
+++ api/Controllers/LemmaController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using api.Models;
+using LatinAutoDecline.Database;
+using LatinAutoDecline.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SQLitePCL;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,72 +12,62 @@ namespace api.Controllers
     [Route("api/lemmas")]
     public class LemmaController : Controller
     {
-        private readonly latinContext _context;
-        public LemmaController(latinContext ctx)
+        private readonly LatinContext _context;
+        private readonly IHelper _helper;
+
+        public LemmaController(LatinContext ctx, IHelper helper)
         {
             _context = ctx;
-
+            _helper = helper;
         }
         #region Only lemma(s)
         // GET api/lemmas/5
         [HttpGet("{id}")]
-        public Lemmas Get(int id)
+        public Lemma Get(int id)
         {
             return _context.Lemmas.FirstOrDefault(l => l.LemmaId == id);
         }
 
         // GET: api/lemmas?ids=41423,41457,42672
         [HttpGet("")]
-        public IEnumerable<Lemmas> Get([ModelBinder(BinderType = typeof(CommaDelimitedArrayModelBinder))] IEnumerable<int> values)
+        public IEnumerable<Lemma> Get([ModelBinder(BinderType = typeof(CommaDelimitedArrayModelBinder))] IEnumerable<int> values)
         {
             if (values == null)
             {
-                return new Lemmas[0];
+                return new Lemma[0];
             }
 
-            var ids = values.ToList();
+            var ids = values;
             return (from l in _context.Lemmas
                     where ids.Contains(l.LemmaId)
-                    select l).ToList();
+                    select l);
         }
         #endregion
         #region Lemma(s) with optionally category (decl, conj etc) and/or forms
-        // GET api/lemmas/extras/5
-        [HttpGet("/extras/{id}")]
-        public Lemmas GetWithExtra(int id, bool data, bool definition, bool forms)
-        {
-            var query = _context.Lemmas;
-            if (data) query.Include(l => l.LemmaData);
-            if (definition) query.Include(l => l.Definition);
-            if (forms) query.Include(l => l.Forms);
-            return query.FirstOrDefault(l => l.LemmaId == id);
-        }
-
         // GET: api/lemmas/extras?ids=41423,41457,42672
-        [HttpGet("/extras")]
-        public IEnumerable<Lemmas> GetMultipleWithExtra([ModelBinder(BinderType = typeof(CommaDelimitedArrayModelBinder))] IEnumerable<int> values, bool data, bool definition, bool forms)
+        [HttpGet("extras")]
+        public IEnumerable<Lemma> GetMultipleWithExtra([ModelBinder(BinderType = typeof(CommaDelimitedArrayModelBinder))] IEnumerable<int> values)
         {
             if (values == null)
             {
-                return new Lemmas[0];
+                return new Lemma[0];
             }
-
             var ids = values.ToList();
-            var query = (from l in _context.Lemmas
-                         where ids.Contains(l.LemmaId)
-                         select l);
-            if (data) query.Include(l => l.LemmaData).ThenInclude(d => d.Category);
-            if (data) query.Include(l => l.LemmaData).ThenInclude(d => d.Gender);
-            if (data) query.Include(l => l.LemmaData).ThenInclude(d => d.PartOfSpeech);
-            if (definition) query.Include(l => l.Definition);
-            if (forms) query.Include(l => l.Forms);
+            return _helper.LoadLemmasWithData(ids);
 
-            return query.ToList();
+        }
+
+        // GET api/lemmas/extras/5
+        [HttpGet("extras/{id}")]
+        public Lemma GetWithExtra(int id)
+        {
+            return _helper.LoadLemmaWithData(id);
         }
         #endregion
-
-
-
-
+        [HttpGet("search")]
+        public IEnumerable<Lemma> Search(string query)
+        {
+            return _context.Lemmas.Where(l => EF.Functions.Like(l.LemmaText,  query));
+        }
     }
 }
