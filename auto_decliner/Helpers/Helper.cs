ccdb65d026b58479fc4b5fc108d31e0d8e61b369
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-01 19:05:36 +0000

    Add helpers to reduce code reuse
    Signed-off-by: Max Cairney-Leeming <mtcairneyleeming@gmail.com>

---
+++ auto_decliner/Helpers/Helper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LatinAutoDecline.Database;
+
+namespace LatinAutoDecline.Helpers
+{
+    public interface IHelper
+    {
+        IEnumerable<Lemma> LoadLemmasWithData(IEnumerable<int> ids);
+        Lemma LoadLemmaWithData(int id);
+    }
+}
