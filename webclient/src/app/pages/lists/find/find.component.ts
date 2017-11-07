ad270564fdbab2786c9a5b94f5677a9e55fcd16a
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-07 20:08:46 +0000

    Added and extended list-finding capabilities

--- webclient/src/app/pages/lists/find/find.component.ts
+++ webclient/src/app/pages/lists/find/find.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Lists} from '../../../models/Lists';
+import {API} from '../../../api/api';
 
 @Component({
   selector: 'app-find',
@@ -6,11 +8,16 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./find.component.css']
 })
 export class FindComponent implements OnInit {
+  public lists: Lists[];
 
-  constructor() {
+  constructor(private http: API) {
   }
 
   ngOnInit() {
+  }
+
+  async makeSearch(search: string) {
+    this.lists = await this.http.searchForList(search);
   }
 
 }
