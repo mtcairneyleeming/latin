8bbc632fc1505b99a90506ea8c1b592608f93043
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-23 21:36:29 +0000

    Add more api methods as needed

--- webclient/src/app/api/api.ts
+++ webclient/src/app/api/api.ts
@@ -26,47 +26,75 @@ export class API {
     }, function (error) {
       return Promise.reject(error);
     });
+    this.http.interceptors.response.use(function (res: ax.AxiosResponse): ax.AxiosResponse | Promise<ax.AxiosResponse> {
+      /*
+      * error handling for all routes:
+      * returns the data object if the response is successful, else returns a nice error with all the data provided
+      * */
+      // handle errors actually returned by the API
+      const result = res.data as md.Result;
+      if (result.success) {
+        res.data = result.data;
+        return res;
+      } else {
+        // i.e. server returned error
+        const err = new Error(result.errorMessage);
+        const promise = new Promise(function (resolve, reject) {
+          reject(err);
+        });
+        return promise;
+      }
+
+    }, function (error) {
+      return Promise.reject(error);
+    });
   }
 
   // Lemma methods
-  public getLemma(id: number): Promise<md.Lemmas> {
+  public getLemma(id: number): Promise<md.Lemma> {
     return this.http.get('/lemmas/' + id)
       .then((data) => {
-        return <md.Lemmas>data.data;
+        return <md.Lemma>data.data;
       });
   }
 
-  public getLemmaWithData(id: number): Promise<md.Lemmas> {
+  public getLemmaWithData(id: number): Promise<md.Lemma> {
     return this.http.get('/lemmas/extras/' + id)
       .then((data) => {
-        return <md.Lemmas>data.data;
+        return <md.Lemma>data.data;
       });
   }
 
   // list methods
-  public getList(id: number): Promise<md.Lists> {
+  public getList(id: number): Promise<md.List> {
     return this.http.get('/lists/' + id)
       .then((data) => {
-        return <md.Lists>data.data.data;
+        return <md.List>data.data;
       });
   }
 
-  public createList(name: string, desc: string): Promise<md.Lists> {
-    return this.http.post('/api/lists', {name: name, description: desc}).then((data) => {
-      return <md.Lists>data.data.data;
+  public createList(name: string, desc: string): Promise<md.List> {
+    const data = {'name': name, 'description': desc};
+    console.log(data);
+    return this.http.post('/lists', data).then((res) => {
+      return <md.List>res.data;
     });
   }
 
-  public async getSectionsInList(id: number): Promise<md.Sections[]> {
-    const sections: md.Sections[] = await this.http.get(`/lists/${id}/sections`)
+  public getMyLists(): Promise<md.List[]> {
+    return this.http.get('/lists/mine').then(res => <md.List[]> res.data);
+  }
+
+  public async getSectionsInList(id: number): Promise<md.Section[]> {
+    const sections: md.Section[] = await this.http.get(`/lists/${id}/sections`)
       .then((data) => {
-        return <md.Sections[]> data.data.data;
+        return <md.Section[]> data.data;
       });
     const sectionPromises = [];
     for (let i = 0; i < sections.length; i++) {
       sectionPromises.push(this.http.get(`/sections/${sections[i].sectionId}/lemmas`)
         .then(async (data) => {
-          const l: md.Lemmas[] = data.data.data;
+          const l: md.Lemma[] = data.data;
           const promises = [];
           for (let j = 0; j < l.length; j++) {
             promises.push(this.getLemmaWithData(l[j].lemmaId));
@@ -90,6 +118,14 @@ export class API {
     return true;
   }
 
+  public getSection(sectionId: number): Promise<md.Section> {
+    return this.http.get('/sections/' + sectionId).then(res => <md.Section> res.data);
+  }
+
+  public getLemmasInSection(sectionId: number): Promise<md.Lemma[]> {
+    return this.http.get('/sections/' + sectionId + '/lemmas').then(res => <md.Lemma[]> res.data);
+  }
+
   // Learning methods
   public async reviseLemma(id: number): Promise<boolean> {
     await this.http.post(`/learn/${id}/revise`).then(console.log);
@@ -99,18 +135,18 @@ export class API {
 
   // searching
 
-  public async searchForLemma(search: string): Promise<md.Lemmas[]> {
+  public async searchForLemma(search: string): Promise<md.Lemma[]> {
     return this.http.get(`/lemmas/search?query=${search}`)
       .then((data) => {
-        return <md.Lemmas[]>data.data;
+        return <md.Lemma[]>data.data;
       });
   }
 
 
-  public async searchForList(search: string): Promise<md.Lists[]> {
+  public async searchForList(search: string): Promise<md.List[]> {
     return this.http.get(`/lists/search?query=${search}`)
       .then((data) => {
-        return <md.Lists[]>data.data;
+        return <md.List[]>data.data;
       });
   }
 }
