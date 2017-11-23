1e1942f676966e17e46ef342934fb59a08a08d3d
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-23 21:45:31 +0000

    Major API changes: Add section load route Add uncompleted import flow Add my-lists feature, incl. search Improve error checking, and change all functions to use Result<generic> type
    Signed-off-by: Max Cairney-Leeming <mtcairneyleeming@gmail.com>

--- api/Controllers/SectionController.cs
+++ api/Controllers/SectionController.cs
@@ -17,7 +17,8 @@ namespace api.Controllers
     public class SectionController : Controller
     {
         private readonly LatinContext _context;
-        private readonly IHelper 
+
+        private readonly IHelper
             _helper;
 
         public SectionController(LatinContext ctx, IHelper helper)
@@ -28,19 +29,36 @@ namespace api.Controllers
         // create new section, but only with the attributes of the section itself
 
         [Authorize]
-        [HttpPost]
-        public Result<Section> Post([FromBody]string name, [FromBody] int listId)
+        [HttpGet("{sectionId}")]
+        public Result<Section> GetById(int sectionId)
         {
-            if (_context.ListUsers.Any(o => o.ListId == listId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
+            var section = _context.Sections.FirstOrDefault(s => s.SectionId == sectionId);
+            if (section != null)
             {
-                var l = new Section { Name = name, ListId = listId };
+                if (_context.Lists.Any(l=> l.ListId == section.ListId && l.IsPrivate == false));
+                {
+                    return new Result<Section>(section);
+                }
+                return new Result<Section>("You must have permission to view that list");
+            }
+            return new Result<Section>("The section must exist to be accessed");
+        }
+        
+        [Authorize]
+        [HttpPost]
+        public Result<Section> Post([FromBody] string name, [FromBody] int listId)
+        {
+            if (_context.ListUsers.Any(o =>
+                o.ListId == listId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
+            {
+                var l = new Section {Name = name, ListId = listId};
                 _context.Sections.Add(l);
                 _context.SaveChanges();
                 return new Result<Section>(l);
             }
             return new Result<Section>("You must have permission to modify that list");
-            
         }
+
         [Authorize]
         [HttpDelete("{sectionId}")]
         public EResult Delete(int sectionId)
@@ -48,9 +66,9 @@ namespace api.Controllers
             var section = _context.Sections.FirstOrDefault(s => s.SectionId == sectionId);
             if (section != null)
             {
-                if (_context.ListUsers.Any(o => o.ListId == section.ListId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
+                if (_context.ListUsers.Any(o =>
+                    o.ListId == section.ListId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
                 {
-
                     _context.Sections.Remove(section);
                     _context.SaveChanges();
                     return new EResult();
@@ -58,16 +76,73 @@ namespace api.Controllers
                 return new EResult("You must have permission to modify that list");
             }
             return new EResult("The section must exist to be deleted");
+        }
 
-            
+        public class LemmaImportModel
+        {
+            public List<string> lemmas { get; set; }
+        }
 
+        /// <summary>
+        /// What we return after an import
+        /// </summary>
+        public class LemmaImportResponseModel
+        {
+            public int SectionId { get; set; }
+
+            /// <summary>
+            /// the lemmas for which import worked - no extra info provided
+            /// </summary>
+            public List<Lemma> SuccessfulImports { get; set; }
+
+            /// <summary>
+            /// lemmas with multiple possible DB values - definitions and classifications are provided to allow the user to choose the right one
+            /// </summary>
+            public List<Lemma> AmbiguousLemmas { get; set; }
+
+            /// <summary>
+            /// Words we have no idea about - ask user to select/type in correct word
+            /// </summary>
+            public List<string> UnknownLemmas { get; set; }
+        }
+
+        // Import a text block of lemmas to a list
+        [Authorize]
+        [HttpPost("{sectionId}/lemmas/import")]
+        public Result<LemmaImportResponseModel> ImportLemmas(int sectionId, [FromBody] LemmaImportModel data)
+        {
+            var lemmasToMatch = data.lemmas;
+            if (lemmasToMatch is null || !lemmasToMatch.Any())
+            {
+                return new Result<LemmaImportResponseModel>("Please provide some data");
+            }
+            var section = _context.Sections.FirstOrDefault(s => s.SectionId == sectionId);
+            if (section is null)
+            {
+                return new Result<LemmaImportResponseModel>(
+                    "The section must exist for you to be able to add lemmas to it");
+            }
+            if (_context.ListUsers.Any(o =>
+                o.ListId == section.ListId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
+            {
+                return new Result<LemmaImportResponseModel>("You must have permission to modifiy this list");
+            }
+            var returnData = new LemmaImportResponseModel();
+            // basic matching - works for "domus", but not "alii... alii"
+            returnData.SuccessfulImports = _context.Lemmas.Where(l => lemmasToMatch.Contains(l.LemmaText)).ToList();
+            foreach (var success in returnData.SuccessfulImports)
+            {
+                lemmasToMatch.Remove(success.LemmaText);
+            }
+            _context.SaveChanges();
+            return new Result<LemmaImportResponseModel>("Not implemented");
         }
 
         public class LemmaIdsModel
         {
             public List<int> ids { get; set; }
         }
-        
+
         [Authorize]
         [HttpPost("{sectionId}/lemmas")]
         public EResult AddLemmasToSection(int sectionId, [FromBody] LemmaIdsModel data)
@@ -77,16 +152,19 @@ namespace api.Controllers
             {
                 Console.WriteLine(ids);
                 return new EResult("Please provide some data");
-
             }
+
             var section = _context.Sections.FirstOrDefault(s => s.SectionId == sectionId);
             if (section is null)
             {
                 return new EResult("The section must exist for you to be able to add lemmas to it");
             }
-            if (_context.ListUsers.Any(o => o.ListId == section.ListId && o.UserId == GetCurrentUser()&& (o.IsContributor || o.IsOwner)))
+
+            if (_context.ListUsers.Any(o =>
+                o.ListId == section.ListId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
             {
-                var existingLemmaIds = _context.SectionWords.Where(s => s.SectionId == sectionId).Select(s => s.LemmaId);
+                var existingLemmaIds =
+                    _context.SectionWords.Where(s => s.SectionId == sectionId).Select(s => s.LemmaId);
                 var newLemmaIds = ids.Distinct().Where(l => !existingLemmaIds.Contains(l));
                 foreach (var id in newLemmaIds)
                 {
@@ -101,6 +179,7 @@ namespace api.Controllers
             }
             return new EResult("You must have permission to modifiy this list");
         }
+
         [Authorize]
         [HttpPatch("{sectionId}/lemmas")]
         public EResult RemoveLemmasFromSection(int sectionId, [FromBody] LemmaIdsModel data)
@@ -110,16 +189,19 @@ namespace api.Controllers
             {
                 Console.WriteLine(ids);
                 return new EResult("Please provide some data");
-
             }
+
             var section = _context.Sections.FirstOrDefault(s => s.SectionId == sectionId);
             if (section is null)
             {
                 return new EResult("The section must exist for you to be able to modify it");
             }
-            if (_context.ListUsers.Any(o => o.ListId == section.ListId && o.UserId == GetCurrentUser()&& (o.IsContributor || o.IsOwner)))
+
+            if (_context.ListUsers.Any(o =>
+                o.ListId == section.ListId && o.UserId == GetCurrentUser() && (o.IsContributor || o.IsOwner)))
             {
-                var lemmasToDelete = _context.SectionWords.Where(w => ids.Contains(w.LemmaId) && w.SectionId == sectionId);
+                var lemmasToDelete =
+                    _context.SectionWords.Where(w => ids.Contains(w.LemmaId) && w.SectionId == sectionId);
                 _context.SectionWords.RemoveRange(lemmasToDelete);
                 _context.SaveChanges();
                 return new EResult(true);
@@ -135,7 +217,6 @@ namespace api.Controllers
             var lemmas = _context.Lemmas.Where(l => lemmaIds.Contains(l.LemmaId));
             return new Result<IEnumerable<Lemma>>(lemmas);
         }
-
 
         private string GetCurrentUser()
         {
