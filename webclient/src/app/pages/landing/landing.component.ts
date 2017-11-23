b3305d48912f3f950261197cb16faaa53260f9f9
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-23 21:41:43 +0000

    Show user's lists on front page

--- webclient/src/app/pages/landing/landing.component.ts
+++ webclient/src/app/pages/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {API} from "../../api/api";
+import {List} from "../../models/List";
 
 @Component({
   selector: 'app-landing',
@@ -7,11 +9,14 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  lists: List[];
 
-  constructor(public auth: AuthService) {
+  constructor(private auth: AuthService,
+              private http: API) {
   }
 
   ngOnInit() {
+    this.http.getMyLists().then(lists => this.lists = lists);
   }
 
 }
