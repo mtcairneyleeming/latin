e5cb9971bfb4f6429319473a2d8ea917225e75d4
Max Cairney-Leeming <mtcairneyleeming@gmail.com>
2017-11-30 20:56:55 +0000

    Add further updates to the auto_decliner
    Signed-off-by: Max Cairney-Leeming <mtcairneyleeming@gmail.com>

---
+++ auto_decliner/Tables/VerbPlurality.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LatinAutoDecline.Tables
+{
+    public struct VerbPlurality
+    {
+        public readonly string First;
+        public readonly string Second; 
+        public readonly string Third;
+
+        public VerbPlurality(string first, string second, string third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(First)}: {First}, {nameof(Second)}: {Second}, {nameof(Third)}: {Third}";
+        }
+    }
+}
