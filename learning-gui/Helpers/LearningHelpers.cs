629e5ffee736576ee851307fc2592875b10119c2
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2019-04-18 11:35:29 +0100

    Improve weighted form selector to choose fewer participles and more verbal forms of a verb
    Signed-off-by: Max Cairney-Leeming <max@cairneyleeming.co.uk>

--- learning-gui/Helpers/LearningHelpers.cs
+++ learning-gui/Helpers/LearningHelpers.cs
@@ -13,7 +13,7 @@ namespace learning_gui.Helpers
 {
     public static class LearningHelpers
     {
-        [NotNull] private static readonly Random _rnd = new Random();
+        [NotNull] private static readonly Random Rnd = new Random();
 
         public static Lemma SelectWord([NotNull] IEnumerable<Lemma> words)
         {
@@ -23,14 +23,15 @@ namespace learning_gui.Helpers
             while (true)
             {
                 var max = words.Max(w => w.UserLearntWord.RevisionStage);
-                currentWord = words.OrderBy(w => _rnd.Next(w.UserLearntWord.RevisionStage, max + 1)).First();
+                currentWord = words.OrderBy(w => Rnd.Next(w.UserLearntWord.RevisionStage, max + 1)).First();
                 if (currentWord.LemmaData is null)
                 {
                     var ok = new Button(3, 14, "Ok")
                     {
                         Clicked = Application.RequestStop
                     };
-                    var dialog = new Dialog($"No data found on lemma {currentWord.LemmaText} ({currentWord.LemmaId})", 70, 7, ok);
+                    var dialog = new Dialog($"No data found on lemma {currentWord.LemmaText} ({currentWord.LemmaId})",
+                        70, 7, ok);
                     Application.Run(dialog);
                     continue;
                 }
@@ -46,7 +47,8 @@ namespace learning_gui.Helpers
         {
             // select forms that aren't blacklisted (any already used) and any indeclinable forms, like adverbs based off a noun
             var forms = context.Forms.Where(f =>
-                    f.LemmaId == l.LemmaId && f.Text != l.LemmaText && !formsToBlacklist.Contains(f.Text) && f.MiscFeatures != "indeclform")
+                    f.LemmaId == l.LemmaId && f.Text != l.LemmaText && !formsToBlacklist.Contains(f.Text) &&
+                    f.MiscFeatures != "indeclform")
                 .ToList();
 
             var treatedAdjAsParticiple = false;
@@ -55,7 +57,8 @@ namespace learning_gui.Helpers
             {
                 // one cause leading to this is that we have selected a ppp like attonitus that has no forms of its own, but attonito does, just its past participle forms.
                 var lemma = context.Lemmas.Single(newL => newL.LemmaId == l.LemmaId);
-                var newId = context.Forms.FirstOrDefault(f => f.Text == lemma.LemmaText && f.MorphCode.StartsWith("t"))?.LemmaId;
+                var newId = context.Forms.FirstOrDefault(f => f.Text == lemma.LemmaText && f.MorphCode.StartsWith("t"))
+                    ?.LemmaId;
                 if (newId is null) return (null, false, false);
 
                 forms = context.Forms.Where(f =>
@@ -72,9 +75,19 @@ namespace learning_gui.Helpers
                 forms = formGroups.Select(g => g.OrderBy(f => f.Text.Length).First()).ToList();
             }
 
+
+            // should never actually get here, as if there are no forms we have already returned unsuccessfully
+            return ChooseFormByWeight(forms, treatedAdjAsParticiple);
+            //return (weightedForms.Select(f => new {f.f, rnd = _rnd.Next() * 1/f.weight}).OrderBy(o => o.rnd).First().f, true, treatedAdjAsParticiple);
+        }
+
+        private static (Form form, bool success, bool treatedAdjAsParticiple) ChooseFormByWeight(List<Form> forms,
+            bool treatedAdjAsParticiple)
+        {
+            forms = forms.GroupBy(f => f.Text).Select(group => group.First()).ToList();
             var weightedForms = forms.Select(f => (weight: GetWeight(f.MorphCode), f)).ToList();
             var totalWeight = weightedForms.Sum(tuple => tuple.weight);
-            double random = _rnd.NextDouble() * totalWeight;
+            var random = Rnd.NextDouble() * totalWeight;
             foreach (var (weight, f) in weightedForms)
             {
                 if (random < weight)
@@ -85,9 +98,7 @@ namespace learning_gui.Helpers
                 random -= weight;
             }
 
-            // should never actually get here, as if there are no forms we have already returned unsuccessfully
             return (null, false, false);
-            //return (weightedForms.Select(f => new {f.f, rnd = _rnd.Next() * 1/f.weight}).OrderBy(o => o.rnd).First().f, true, treatedAdjAsParticiple);
         }
 
         private static double GetWeight(string morphCode)
@@ -99,16 +110,17 @@ namespace learning_gui.Helpers
 
             if (MorphCodeParser.ParsePartOfSpeech(morphCode) == Part.Participle)
             {
-                if (MorphCodeParser.ParseTense(morphCode) == Tense.Present) weight = 0.3;
-                if (MorphCodeParser.ParseTense(morphCode) == Tense.Perfect) weight = 0.4;
-                if (MorphCodeParser.ParseTense(morphCode) == Tense.Future) weight = 0.4;
+                if (MorphCodeParser.ParseTense(morphCode) == Tense.Present) weight = 0.7;
+                if (MorphCodeParser.ParseTense(morphCode) == Tense.Perfect) weight = 0.7;
+                if (MorphCodeParser.ParseTense(morphCode) == Tense.Future) weight = 0.7;
             }
 
             if (MorphCodeParser.ParsePartOfSpeech(morphCode) == Part.Verb)
             {
-                if (MorphCodeParser.ParseMood(morphCode) == Mood.Gerundive) weight = 0.5;
-                if (MorphCodeParser.ParseMood(morphCode) == Mood.Subjunctive) weight *= 1.5;
-                if (MorphCodeParser.ParseMood(morphCode) == Mood.Infinitive) weight = 1.0;
+                if (MorphCodeParser.ParseMood(morphCode) == Mood.Gerundive) weight = 0.6;
+                if (MorphCodeParser.ParseMood(morphCode) == Mood.Subjunctive) weight *= 1.7;
+                if (MorphCodeParser.ParseMood(morphCode) == Mood.Infinitive &&
+                    MorphCodeParser.ParseTense(morphCode) != Tense.Present) weight = 1.8;
                 if (MorphCodeParser.ParseMood(morphCode) == Mood.Imperative) weight = 1.0;
             }
 
@@ -117,7 +129,7 @@ namespace learning_gui.Helpers
 
         public static string SplitTextIntoLines(string text, int width)
         {
-            return String.Join("\n", Regex.Matches(text, $".{{1,{width}}}").Select(m => m.Value));
+            return string.Join("\n", Regex.Matches(text, $".{{1,{width}}}").Select(m => m.Value));
         }
     }
 }
